6efd38bc0eccfdb5d3a8ec51fccb1ab699fe08fb
Kenavo29 <123992839+Kenavo29@users.noreply.github.com>
2023-05-03 17:44:45 +0200

    Ajout UI plutot pas piquée des hannetons sur la scène testAntoine

--- Assets/MainCamera.cs
+++ Assets/MainCamera.cs
@@ -10,6 +10,7 @@ public class MainCamera : MonoBehaviour
     public float farClipPlaneDistance = 100f;
     public float nearCutPlane = 30;
     public float farCutPlane = 40;
+    public bool cutPlane = false;
 
     // Start is called before the first frame update
     void Start()
@@ -47,4 +48,47 @@ public class MainCamera : MonoBehaviour
 
     }
 
+    public void ButtonCuttingPlane()
+    {
+
+        if (!cutPlane)
+        {
+            activateCuttingPlane();
+            cutPlane = true;
+        }
+        else
+        {
+            disableCuttingPlane();
+            cutPlane = false;
+        }
+    }
+
+
+    public void activateCuttingPlane()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        transform.position += direction * moveSpeed * Time.deltaTime;
+
+        // Récupérer la caméra attachée à cet objet
+        Camera camera = GetComponent<Camera>();
+
+        // Modifier la distance du plan de coupe proche (near clip plane)
+        camera.nearClipPlane = nearCutPlane;
+
+        // Modifier la distance du plan de coupe éloigné (far clip plane)
+        camera.farClipPlane = farCutPlane;
+    }
+
+    public void disableCuttingPlane()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        transform.position += direction * moveSpeed * Time.deltaTime;
+        Camera camera = GetComponent<Camera>();
+        camera.nearClipPlane = nearClipPlaneDistance;
+        camera.farClipPlane = farClipPlaneDistance;
+    }
 }
